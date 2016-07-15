6929cd30774328ba446cb5eda01d6fa86d904d37
koyot9110 <jozeflukac91@gmail.com>
2016-07-15 15:09:16 +0200

    gs

---
+++ src/sk/ness/jpa/Student.java
@@ -0,0 +1,50 @@
+package sk.ness.jpa;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
+@Entity
+public class Student {
+	
+	@Id
+	@GeneratedValue
+	private int ident;
+	
+	private String meno;
+	
+	private String priezvisko;
+	
+	private int vek;
+	
+	public int getIdent() {
+		return ident;
+	}
+	public void setIdent(int ident) {
+		this.ident = ident;
+	}
+	public String getMeno() {
+		return meno;
+	}
+	public void setMeno(String meno) {
+		this.meno = meno;
+	}
+	public String getPriezvisko() {
+		return priezvisko;
+	}
+	public void setPriezvisko(String priezvisko) {
+		this.priezvisko = priezvisko;
+	}
+	public int getVek() {
+		return vek;
+	}
+	public void setVek(int vek) {
+		this.vek = vek;
+	}
+
+	@Override
+	public String toString() {
+		return "Student [ident=" + ident + ", meno=" + meno + ", priezvisko="
+				+ priezvisko + ", vek=" + vek + "]";
+	}
+}
---
+++ src/sk/ness/jpa/Test.java
@@ -0,0 +1,35 @@
+package sk.ness.jpa;
+
+import javax.persistence.Query;
+import javax.persistence.EntityManager;
+
+public class Test {
+
+	public static void main(String[] args) throws Exception {
+
+		Student student = new Student();
+		student.setMeno("Ferko");
+		student.setPriezvisko("Hrasko");
+		student.setVek(18);
+
+		JpaHelper.beginTransaction();
+		EntityManager em = JpaHelper.getEntityManager();
+		em.persist(student);
+		JpaHelper.commitTransaction();
+
+		Query query = em
+				.createQuery("SELECT s FROM Student s WHERE s.meno=:meno");
+		query.setParameter("meno", "Ferko");
+		System.out.println(query.getResultList());
+
+		JpaHelper.beginTransaction();
+		student = em.find(Student.class, 5);
+		student.setVek(20); 
+//		em.remove(student);
+		JpaHelper.commitTransaction();
+		System.out.println(student);
+		JpaHelper.closeAll();
+
+	}
+
+}
